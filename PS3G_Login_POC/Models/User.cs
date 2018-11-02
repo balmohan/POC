f43e5de618b1150b2a310d3ce29652de0f47a35a
balmohan <balmohan.job@gmail.com>
2018-11-02 18:48:06 +0530

    minor changes

--- PS3G_Login_POC/Models/User.cs
+++ PS3G_Login_POC/Models/User.cs
@@ -7,7 +7,6 @@ namespace PS3G_Login_POC.Models
 {
     public class User
     {
-        public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
     }
